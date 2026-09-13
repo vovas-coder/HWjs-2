58ec22e6cbd0543b2e8c3673640568a81085f1f7
vovas-coder <volodyapyrch2012@gmail.com>
2026-09-13 21:08:26 +0300

    lol

---
+++ js.js
@@ -0,0 +1,65 @@
+const celsius = 25;
+let fahrenheit = celsius * 9 / 5 + 32;
+
+console.log(fahrenheit);
+
+
+const daysInMonth = 30;
+const hoursInMonth = 24;
+const minutesInMonth = 1440;
+
+console.log(daysInMonth * hoursInMonth);
+console.log(daysInMonth * minutesInMonth);
+
+
+let healt = 100;
+let energy = 100;
+
+healt = healt - 50;
+energy = energy - 50;
+
+console.log(healt);
+console.log(energy);
+
+
+const totalPrice = 1000;
+const discountedPrice = totalPrice * 0.9;
+
+console.log(discountedPrice);
+
+
+const floatNumber = 12.78;
+const roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
+
+
+const floatString  = 45.67;
+const parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
+
+
+const intString = "123";
+
+const parsedInt = parseInt(intString);
+
+console.log(parsedInt);
+
+
+const number = 64;
+
+const sqrtNumber = Math.sqrt(number);
+
+console.log(sqrtNumber);
+
+
+const integer = 42;
+const stringNumber = "256";
+
+const convertedInt = parseInt(stringNumber);
+
+console.log(convertedInt);
+
+
+const convertedString = integer.toString();
+
+console.log(convertedString);
